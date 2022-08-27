b07e4164cf91ab655eac14907bb46ca296b442a1
ROHIT32767 <rohitgowlapalli01@gmail.com>
2022-08-27 17:06:38 +0530

    hi

--- prompt.c
+++ prompt.c
@@ -2,9 +2,13 @@
 #include "headers.h"
 typedef long long int INT;
 void prompt() {
-   INT display_size=256;
+   INT display_size=1000;
    // printf("OS & NW TAs rock: ");   
    char* display = (char*)calloc(display_size,sizeof(char));
-   
-
+   char* user_name=(char*)calloc(200,sizeof(char));
+   char* system_name=(char*)calloc(200,sizeof(char));
+   char* curr_dir=(char*)calloc(200,sizeof(char));
+   user_name=get_pwnam();
+   sprintf(display,"%s@%s:%s->",user_name,system_name,curr_dir);
+   write(1,display, strlen(display));
 }
